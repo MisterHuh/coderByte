c43a6858a327a7e2631b659e7e21ce79c3d8b916
MisterHuh <jaehuh86@yahoo.com>
2019-11-25 12:55:44 -0800

    still WIP

---
+++ longestWord.js
@@ -0,0 +1,49 @@
+function LongestWord(sen) {
+
+  var strings = sen.split(" ");
+  console.log("strings is: ", strings);
+
+  var answerArray = [];
+  stringsLength = [];
+  var answer = null;
+  var answreIndex = null;
+
+  for (var index = 0; index < strings.length; index++) {
+    var testObj = {
+      word: strings[index],
+      length: strings[index].length
+    }
+    answerArray.push(testObj);
+    stringsLength.push(strings[index].length);
+
+    if (index === strings.length - 1) {
+      answer = Math.max.apply(Math, stringsLength);
+      for (var testIndex = 0; testIndex < strings.length; testIndex++) {
+        if (answer == stringsLength[index]) {
+          console.log("the final answer is: ", answerArray[index]["word"]);
+        }
+      }
+    }
+  }
+
+  console.log("stringsLength; is: ", stringsLength);
+  console.log("answerArray; is: ", answerArray);
+  console.log("answer is: ", answer);
+  console.log("answerArray[2]'name']", answerArray[2]['name'])
+
+
+}
+
+
+/*
+
+Longest Word
+Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string.
+If there are two or more words that are the same length, return the first word from the string with that length.
+Ignore punctuation and assume sen will not be empty.
+
+Examples
+Input: "fun time"
+Output: time
+
+*/
