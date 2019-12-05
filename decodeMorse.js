0c399528b49080806d60c4bf7ac3e13b0197d5ff
MisterHuh <jaehuh86@yahoo.com>
2019-12-04 22:06:04 -0800

    WIP decodeMorse

---
+++ decodeMorse.js
@@ -0,0 +1,9 @@
+$(document).ready(decodeMorse('....   .   -.--'));
+
+// H E Y
+
+function decodeMorse(morseCode) {
+  var test = morseCode.split("   ");
+  console.log("test is: ", test);
+  //your code here
+}
